8ecf5964d659df335dfd54ebf21010cfb5aa86a2
Christian <chris@polarchris.com>
2025-04-03 12:28:22 +0200

    statements challenge task 3 completed

--- js-conditions-and-booleans/js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_statements/js/index.js
@@ -12,11 +12,66 @@ if (receivedPassword == SUPER_SECRET_PASSWORD) {
 }
 
 // Part 2: Even / Odd
-const number = 6;
+const number = 5;
+
+if (number % 2 === 1) {
+  console.log("The number is uneven.");
+} else {
+  console.log("The number is even.");
+}
 
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+const numberOfHotdogs = 1000005;
 
+if (numberOfHotdogs < 5) {
+  let priceSingle = 2;
+  let priceTotal = priceSingle * numberOfHotdogs;
+
+  console.log(
+    "Preis p. Hotdog: " +
+      priceSingle +
+      " €" +
+      "Gesamtpreis: " +
+      priceTotal +
+      " €"
+  );
+} else if (numberOfHotdogs < 100) {
+  let priceSingle = 1.5;
+  let priceTotal = priceSingle * numberOfHotdogs;
+
+  console.log(
+    "Preis p. Hotdog: " +
+      priceSingle +
+      " €" +
+      "  Gesamtpreis: " +
+      priceTotal +
+      " €"
+  );
+} else if (numberOfHotdogs < 1000000) {
+  let priceSingle = 1;
+  let priceTotal = priceSingle * numberOfHotdogs;
+
+  console.log(
+    "Preis p. Hotdog: " +
+      priceSingle +
+      " €" +
+      "  Gesamtpreis: " +
+      priceTotal +
+      " €"
+  );
+} else if (numberOfHotdogs >= 1000000) {
+  let priceSingle = 0.1;
+  let priceTotal = priceSingle * numberOfHotdogs;
+
+  console.log(
+    "Preis p. Hotdog: " +
+      priceSingle +
+      " €" +
+      "  Gesamtpreis: " +
+      priceTotal +
+      " €"
+  );
+}
 // Part 4: Daytime
 const currentHour = 12;
 
