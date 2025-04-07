cbef82d15016fc6c999a0ac24177c10eff603103
Christian <chris@polarchris.com>
2025-04-07 14:51:57 +0200

    Challenge 1 input-to-uppercase completed

--- JS-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ JS-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,9 @@
 console.clear();
+const firstInput = document.querySelector('[data-js="first-input"]');
+const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+
+uppercaseButton.addEventListener("click", () => {
+  firstInput.value = firstInput.value.toUpperCase();
+});
+
+console.log(FirstInput.toUpperCase);
