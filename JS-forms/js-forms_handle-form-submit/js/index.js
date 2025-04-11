040894043ed6f4401df7c6eab546fac1c9ef5713
Christian <chris@polarchris.com>
2025-04-11 11:15:46 +0200

    saving actual work status

--- JS-forms/js-forms_handle-form-submit/js/index.js
+++ JS-forms/js-forms_handle-form-submit/js/index.js
@@ -1,67 +1,13 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
-const FirstNameInput = document.getElementById("first-name");
-const LastNameInput = document.getElementById("last-name");
-const AgeInput = document.getElementById("age");
-const EmailInput = document.getElementById("email");
-const ComplaintTypeInput = document.getElementById("complaint");
-const ComplaintDetailsInput = document.getElementById("details");
-const BadnessRatingInput = document.getElementById("badness");
-const OrderDateInput = document.getElementById("order-date");
-const TosInput = document.getElementById("tos");
 
-window.addEventListener("DOMContentLoaded", () => {
-  FirstNameInput.focus();
+form.addEventListener("submit", function getFormContent(event) {
+  console.log(event);
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  event.target.reset();
+  event.target.elements.firstName.focus();
 });
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  validateInputs();
-});
-
-const validateInputs = () => {
-  const FirstName = FirstNameInput.value.trim();
-  const LastName = LastNameInput.value.trim();
-  const Age = parseInt(AgeInput.value.trim());
-  const Email = EmailInput.value.trim();
-  const ComplaintType = ComplaintTypeInput.value.trim();
-  const ComplaintDetails = ComplaintDetailsInput.value.trim();
-  const BadnessRating = parseInt(BadnessRatingInput.value, 10);
-  const OrderDate = OrderDateInput.value;
-  const TOS = TosInput.checked;
-
-  if (
-    !FirstName ||
-    !LastName ||
-    !Email ||
-    !ComplaintType ||
-    !OrderDate ||
-    !TOS
-  ) {
-    alert(
-      "Please enter your First Name, Last Name, Email, What is wrong? and your order date before you proceed. We also need you to accept our terms of service. Thank you!"
-    );
-    return;
-  }
-
-  console.log({
-    FirstName,
-    LastName,
-    Age,
-    Email,
-    ComplaintType,
-    ComplaintDetails,
-    BadnessRating,
-    OrderDate,
-    TOS,
-  });
-
-  const AgeBadnessSum = Age + BadnessRating;
-  console.log("The age-badness-sum of " + FirstName + " is " + AgeBadnessSum);
-
-  form.reset();
-  setTimeout(() => {
-    FirstNameInput.focus();
-  }, 0);
-};
--- js-loops/js-loops_forin-select-nav/js/index.js
+++ js-loops/js-loops_forin-select-nav/js/index.js
@@ -21,7 +21,15 @@ select.name = "languages";
 main.append(select);
 
 // --v-- write or modify code below this line --v--
+for (const language in languages) {
+  console.log(languages[language]);
 
+  const option = document.createElement("option");
+  option.value = languages[language];
+  option.textContent = languages[language];
+  select.append(option);
+  console.log(option);
+}
 // --^-- write or modify code above this line --^--
 
 // Part 2: Creating a Navigation Bar
@@ -38,5 +46,28 @@ main.append(navElement);
 navElement.append(ul);
 
 // --v-- write or modify code below this line --v--
+for (const item in nav) {
+  console.log(nav[item]);
 
+  const navObject = nav[item];
+  const menuItems = [];
+
+  for (const subItem in navObject) {
+    console.log(navObject[subItem]);
+    const list = navObject[subItem];
+    menuItems.push(list);
+  }
+
+  console.log("text:" + menuItems);
+  const navItem = document.createElement("li");
+
+  console.log(navItem);
+
+  const navLink = document.createElement("a");
+  navLink.href = menuItems[0];
+  navLink.textContent = menuItems[1];
+  navItem.append(navLink);
+  console.log(navItem);
+  ul.append(navItem);
+}
 // --^-- write or modify code above this line --^--
