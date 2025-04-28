634098fe0e81df829137944fcfd885e6ca796b9a
Christian <chris@polarchris.com>
2025-04-28 12:36:28 +0200

    Completed task 3 iss-tracker

--- react-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
+++ react-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
@@ -11,7 +11,26 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  async function getISSCoords() {
+    const response = await fetch(
+      "https://api.wheretheiss.at/v1/satellites/25544"
+    );
+    const data = await response.json();
+    setCoords({
+      longitude: data.longitude,
+      latitude: data.latitude,
+    });
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      getISSCoords();
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <main>
