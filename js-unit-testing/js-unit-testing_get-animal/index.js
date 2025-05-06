fd4d5b5a4ce98242c5f347c07146b4f7a1a162c7
Polarchris <chris@polarchris.com>
2025-05-06 15:44:10 +0200

    Completed challenge 1 get-animal

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,7 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  return !animal
+    ? "I do not like animals at all!"
+    : animal === "cats"
+    ? "I totally love cats!"
+    : `I like ${animal}!`;
+}
